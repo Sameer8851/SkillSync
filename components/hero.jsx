9e916246e261886069bde425bc2855cb9910cd61
Sameer <sameerchaudhary8851@gmail.com>
2025-04-01 17:58:24 +0530

    Created Landing Page

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -7,6 +8,29 @@ import Image from 'next/image'
 
 
 const HeroSection = () => {
+
+const imageRef = useRef(null);
+
+useEffect(()=>{
+    const imageElement = imageRef.current;
+
+    const handleScroll = () =>{
+        const scrollPostion = window.scrollY;
+        const scrollThreshold = 100;
+    
+        if(scrollPostion > scrollThreshold){
+            imageElement.classList.add("scrolled")
+        }else{
+            imageElement.classList.remove("scrolled")
+        }
+    
+    }
+    window.addEventListener("scroll" , handleScroll)
+    return () => window.removeEventListener("scroll",handleScroll)
+    
+},[])
+
+
   return (
     <section className='w-full pt-36 md:pt-48 pb-10' >
         <div className='space-y-6 text-center'>
@@ -27,8 +51,8 @@ const HeroSection = () => {
                 </Link>
             </div>
 
-            <div>
-                <div>
+            <div className='hero-image-wrapper mt-5 md:mt-0' >
+                <div ref={imageRef} className='hero-image' >
                     <Image
                     src={"/banner.jpeg"}
                     width={1280}
