625ecbcdb168bcccc577853d88229c8de9ce5395
Sameer <sameerchaudhary8851@gmail.com>
2025-04-10 22:35:25 +0530

    Onboarding Page

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -5,11 +5,29 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { onboardingSchema } from "@/app/lib/schema";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
 
 const OnboardingForm = ({ industries }) => {
-
-const [selectedIndustry,setselectedIndustry] = useState(null);
-const router= useRouter()
+  const [selectedIndustry, setselectedIndustry] = useState(null);
+  const router = useRouter();
 
   const {
     register,
@@ -20,7 +38,141 @@ const router= useRouter()
   } = useForm({
     resolver: zodResolver(onboardingSchema),
   });
-  return <div></div>;
+
+const onSubmit = async (values) => {};  
+
+  const watchIndustry = watch("industry");
+
+  return (
+    <div className="flex items-center justify-center bg-background">
+      <Card className="w-full max-w-lg mt-10 mx-2">
+        <CardHeader>
+          <CardTitle className="gradient-title text-4xl">
+            Complete Your Profile
+          </CardTitle>
+          <CardDescription>
+            Select your industry to get personalized career insights and
+            recommendations.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)} >
+            <div className="space-y-2">
+              <Label htmlFor="industry">Industry</Label>
+              <Select
+                onValueChange={(value) => {
+                  setValue("industry", value);
+                  setselectedIndustry(
+                    industries.find((ind) => ind.id == value)
+                  );
+                  setValue("subIndustry", "");
+                }}
+              >
+                <SelectTrigger id="industry" className="w-full">
+                  <SelectValue placeholder="Select an industry" />
+                </SelectTrigger>
+                <SelectContent>
+                  {industries.map((ind) => {
+                    return (
+                      <SelectItem value={ind.id} key={ind.id}>
+                        {ind.name}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectContent>
+              </Select>
+              {errors.industry && (
+                <p className="text-sm text-red-500">
+                  {errors.industry.message}
+                </p>
+              )}
+            </div>
+
+
+            {watchIndustry && (
+              <div className="space-y-2">
+              <Label htmlFor="subIndustry" >Specialization</Label>
+            <Select
+             onValueChange={(value)=> setValue("subIndustry",value)}
+            >
+              <SelectTrigger id="subIndustry" className="w-full">
+                <SelectValue placeholder="Select an industry" />
+              </SelectTrigger>
+              <SelectContent>
+               {selectedIndustry?.subIndustries.map((ind)=>{
+                return <SelectItem value={ind} key={ind} >{ind}</SelectItem>
+               })}
+              </SelectContent>
+            </Select>
+            {errors.subIndustry && (
+              <p className="text-sm text-red-500">
+                {errors.subIndustry.message}
+              </p>
+            )}
+            </div>
+          )}
+
+            <div className="space-y-2">
+              <Label htmlFor="experience">Year of Experience</Label>
+              <Input
+              id="experience"
+              type="number"
+              min="0"
+              max="50"
+              placeholder="Enter years of experience"
+              {...register("experience")}
+              />
+
+              {errors.experience && (
+                <p className="text-sm text-red-500">
+                  {errors.experience.message}
+                </p>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="skills">Skills</Label>
+              <Input
+              id="skills"
+              placeholder="e.g., Python, JavaScript, Project Management"
+              {...register("skills")}
+              />
+              <p className="text-sm text-muted-foreground">
+                Seperate multiple skills with commas
+              </p>
+
+              {errors.skills && (
+                <p className="text-sm text-red-500">
+                  {errors.skills.message}
+                </p>
+              )}
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="bio">Professional Bio</Label>
+              <Textarea
+              id="bio"
+              placeholder="Tell us about your professional background..."
+              className="h-32"
+              {...register("bio")}
+              />
+             
+
+              {errors.bio && (
+                <p className="text-sm text-red-500">
+                  {errors.bio.message}
+                </p>
+              )}
+            </div>
+
+            <Button type="submit" className="w-full">
+              Complete Profile
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default OnboardingForm;
--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { industries } from "./../../../data/industries";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
+import OnboardingForm from "./_components/onboarding-form";
+
+
 
 export default async function OnboardingPage() {
   // Check if user is already onboarded
   const { isOnboarded } = await getUserOnboardingStatus();
 
-  if (isOnboarded) {
-    redirect("/dashboard");
-  }
+  // if (isOnboarded) {
+  //   redirect("/dashboard");
+  // }
 
   return (
     <main>
